37823c7a0b4ebf9707c672dc124cc15596bbdb9d
ot <o@o>
2021-12-16 06:35:57 -0500

    Initial Commit

---
+++ Demo ObservedObject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo ObservedObject
+//
+//  Created by ot on 16.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo ObservedObject/Demo_ObservedObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_ObservedObjectApp.swift
+//  Demo ObservedObject
+//
+//  Created by ot on 16.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Demo_ObservedObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
